3c7f607f363532c0f6225822e5d9e96ad1f727df
Danton Nguyen <dantonnguyen88@gmail.com>
2017-02-26 04:21:02 +0000

    revised

--- express_server.js
+++ express_server.js
@@ -1,17 +1,18 @@
-
+0
 // listen to port 8080
 // need http
 
 var express = require('express');
 var cookieParser = require('cookie-parser');
-var app = express();
 var bcrypt = require('bcrypt');
+var app = express();
 var PORT = process.env.PORT || 8080;
 
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static(__dirname + '/public'));
-app.use(cookieParser());
 var cookieSession = require('cookie-session');
 
 app.use(cookieSession({
@@ -28,15 +29,13 @@ let urlDatabase = {
     url: "http://www.lighthouselabs.ca",
     userID: "abc123",
     allCount: 0,
-    uniqueCount: 0,
-    timeStamp: 0
+    uniqueCount: 0
   },
   "9sm5xK": {
     url: "http://www.google.com",
     userID: "abc123",
     allCount: 0,
-    uniqueCount: 0,
-    timeStamp: 0
+    uniqueCount: 0
   }
 };
 
@@ -60,7 +59,7 @@ const users = {
 
 // Middleware
 
-app.use(function(req, res, next){
+app.use(function(req, res, next) {
   res.locals.user_id = req.session.user_id;
   res.locals.email = req.session.user_id;
   res.locals.users = users;
@@ -74,15 +73,15 @@ function generateRandomString() {
   var text = "";
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for(var i = 0; i < 5; i++ ) {
+  for (var i = 0; i < 5; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return text;
 }
 
-function emailExists(emailReg){
-  for(userid in users) {
-    if(users[userid]['email'] === emailReg){
+function emailExists(emailReg) {
+  for (userid in users) {
+    if (users[userid]['email'] === emailReg) {
       return userid;
     }
   }
@@ -92,17 +91,32 @@ function emailExists(emailReg){
 function urlsForUser(id) {
   var idArr = [];
 
-  for(let url in urlDatabase) {
-    var shortURL = urlDatabase[url];
-
-    for(let userID in shortURL){
-      if(id === shortURL[userID]) {
-        idArr.push(url);
-      }
+  for (let url in urlDatabase) {
+    var shortURL = urlDatabase[url].userID;
+    if (id === shortURL) {
+      idArr.push(url);
     }
   }
   return idArr;
 }
+
+let checkValidURL = (url) => {
+  if (/^\s+$/.test(url) || url === null || !url) {
+    return false;
+  } else {
+    return true;
+  }
+};
+let includesHTTP = (url) => {
+
+  console.log(url);
+  if (url.includes('http://')) {
+    return url;
+  } else {
+    return ('http://' + url);
+  }
+
+};
 
 // ROOT
 
@@ -115,23 +129,18 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World! TESTSES</b></body></html>\n");
-});
 
 // Access /urls
 
 app.get("/urls", (req, res) => {
-  if(!req.session.user_id) {
+  if (!req.session.user_id) {
     res.status(401).send('Unauthorized');
   } else {
 
-    let templateVars = {userUrls: (urlsForUser(req.session.user_id))};
-    console.log(urlDatabase);
+    let templateVars = {
+      userUrls: (urlsForUser(req.session.user_id))
+    };
     res.render("urls_index", templateVars);
   }
 });
@@ -145,14 +154,23 @@ app.get("/new", (req, res) => {
 // Create new URL Handler
 
 app.post("/urls", (req, res) => {
-  if(req.session.user_id){
+  let valid = checkValidURL(req.body.newURL)
+  if (valid) {
+    console.log(valid);
+    } else {
+    res.redirect(`/urls`)
+    return;
+  }
+
+  if (req.session.user_id && valid) {
+    let validatedURL = includesHTTP(req.body.newURL);
     let newShortURL = generateRandomString();
     urlDatabase[newShortURL] = {
-      url: req.body.newURL,
+      url: validatedURL,
       userID: req.session.user_id,
       allCount: 0,
       uniqueCount: 0,
-      timeStamp: new Date()
+      timeStamp: new Date().toUTCString()
     };
     res.redirect(`/urls/${newShortURL}`);
   } else {
@@ -163,7 +181,7 @@ app.post("/urls", (req, res) => {
 /////////////////////////////////// LOGIN \\\\\\\\\\\\\\\\\\\\\\\\ TASK 6
 
 app.get("/login", (req, res) => {
-  if(req.session.user_id) {
+  if (req.session.user_id) {
     res.redirect('/urls');
   } else {
     res.render('urls_login');
@@ -172,22 +190,21 @@ app.get("/login", (req, res) => {
 
 app.post("/login", (req, res) => {
 
-  if(!req.body['user_id']) {
-    res.send('invalid user id');
+  if (!req.body.userEmail) {
+    res.send("Invalid credentials. <a href='/login'>Try again!</a>");
   }
 
-  if(emailExists(req.body.user_id)) {
-    req.session.user_id = emailExists(req.body.user_id);
-  } else {
-  res.status(403).send('Email does not exist!');
+  if (!emailExists(req.body.userEmail)) {
+    res.status(403).send("Email does not exist! <a href='/login'>Try again!</a>");
   }
 
-  if(bcrypt.compare(req.body['password'], users[emailExists(req.body['user_id'])]['password'])) {
+  if (bcrypt.compareSync(req.body.password, users[emailExists(req.body.userEmail)].password)) {
 
+    req.session.user_id = req.body.userEmail;
     res.redirect('/urls');
 
   } else {
-    res.status(403).send('Invalid password');
+    res.status(403).send("Invalid password<a href='/login'>Try again!</a>");
   }
 
 });
@@ -197,8 +214,8 @@ app.post("/login", (req, res) => {
 
 
 app.get("/register", (req, res) => {
-  if(req.session.user_id){
-    res.redirect('/')
+  if (req.session.user_id) {
+    res.redirect('/');
   }
   res.render('urls_register');
 });
@@ -206,14 +223,14 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
   var email = req.body['email'];
 
-  var password = bcrypt.hashSync(req.body['password'], 10);
+  var password = bcrypt.hashSync(req.body.password, 10);
   var userID = generateRandomString();
 
-  if(!email || !password) {
-    res.status(400).send("Invalid input!");
+  if (!email || !password) {
+    res.status(400).send("Invalid input!<a href='/register'>Try again!</a>");
   } else if (emailExists(email)) {
 
-    res.status(400).send("E-mail already exists!");
+    res.status(400).send("E-mail already exists!<a href='/register'>Try again!</a>");
 
   } else {
     users[userID] = {
@@ -222,7 +239,7 @@ app.post("/register", (req, res) => {
       password
     };
     req.session.email = email;
-    req.session.user_id = userID;
+    req.session.user_id = email;
     res.redirect('/urls');
   }
 });
@@ -235,6 +252,7 @@ app.post("/logout", (req, res) => {
   req.session = null;
   res.redirect('/login');
 });
+
 /////////// UPDATE URL
 app.get("/urls/:id", (req, res) => {
   let shortURL = req.params.id;
@@ -243,13 +261,13 @@ app.get("/urls/:id", (req, res) => {
     longURL: urlDatabase[shortURL]['url']
   };
 
-  if(!shortURL){
-    res.status(404).send("URL does not exist!");
+  if (!shortURL) {
+    res.status(404).send("URL does not exist!<a href='/urls'>Try Again!</a>");
   }
-  if(!req.session.user_id) {
+  if (!req.session.user_id) {
     res.status(401).send("Please login <a href='/login'>Click Here</a>");
   } else if (req.session.user_id !== urlDatabase[shortURL]['userID']) {
-    res.status(403).send("Invalid Permissions!");
+    res.status(403).send("Invalid credentials!<a href='/urls'>Try again!</a>");
   }
   res.render("urls_show", templateVars);
 });
@@ -257,40 +275,38 @@ app.get("/urls/:id", (req, res) => {
 //////////////////////// Update URL
 
 app.post("/urls/:id", (req, res) => {
-  if(!req.params.id) {
-    res.status(404).send("URL does not exist!");
+  if (!req.params.id) {
+    res.status(404).send("URL does not exist!<a href='/urls'>Go back!</a>");
   }
-  if(!req.session.user_id) {
+  if (!req.session.user_id) {
     res.status(401).send("Please login <a href='/login'>Click Here</a>");
   }
-  if(req.session.user_id !== urlDatabase[req.params.id].userID){
-    res.status(403).send("Error 403, Unauthorized access.");
+  if (req.session.user_id !== urlDatabase[req.params.id].userID) {
+    res.status(403).send("Error 403, Unauthorized access. <a href='/urls'>Home</a>");
   }
 
   let updateURL = req.body;
   let shortURL = req.params.id;
-  console.log('POSTLINK:::::::::', urlDatabase[shortURL]);
   urlDatabase[shortURL].url = updateURL.longURL;
+  res.redirect('/urls');
 
   console.log(urlDatabase);
 });
 
 
 app.get("/u/:id", (req, res) => {
-  if(req.params.id){
-    let longURL = urlDatabase[req.params.id]['url'];
+  if (req.params.id && urlDatabase[req.params.id].url) {
+    let longURL = urlDatabase[req.params.id].url;
 
-
-    var views = urlDatabase[req.params.id]['allCount'];
+    var views = urlDatabase[req.params.id].allCount;
     views++;
-    urlDatabase[req.params.id]['allCount'] = views;
+    urlDatabase[req.params.id].allCount = views;
     urlDatabase[req.params.id].count += 1;
 
-    console.log('REQID >>>>>>>>>>>>>>>>>', req.session.sessionid);
 
     res.redirect(longURL);
   } else {
-    res.status(404).send("Short URL doesn't exist");
+    res.status(404).send("Short URL doesn't exist! Sorry!");
   }
 });
 
@@ -298,11 +314,11 @@ app.get("/u/:id", (req, res) => {
 
 app.post("/urls/:id/delete", (req, res) => {
 
-  if(req.session.user_id === urlDatabase[req.params.id]['userID']){
+  if (req.session.user_id === urlDatabase[req.params.id]['userID']) {
     delete urlDatabase[req.params.id];
     res.redirect("/urls");
   }
 
-  res.status(403).send('No Permission!');
+  res.status(403).send('No Permission! <a href="/urls">Go back!</a>');
 
 });
